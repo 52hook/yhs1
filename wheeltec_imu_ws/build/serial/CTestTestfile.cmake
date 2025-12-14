9bceb3598dbf3b338ff833c8bf754c4b59f547a2
Li Jiahao <148352705+52hook@users.noreply.github.com>
2025-12-14 14:13:22 +0800

    Delete wheeltec_imu_ws directory

--- wheeltec_imu_ws/build/serial/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/wheeltec_imu_ws/src/serial
-# Build directory: /home/nvidia/wheeltec_imu_ws/build/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
